50370986c3ff23fa66a88d323f6d9652fe38194b
Thabo Matjuda <thabo@anylytical.co.za>
2023-07-23 00:00:13 +0200

    1. Build script for both Windows & Mac OSes

--- dev-tools/build-app.sh
+++ dev-tools/build-app.sh
@@ -4,18 +4,23 @@
 # BASE CONFIGURATIONS
 # =====================================================================================================================
 if [ -z "${BASH_VERSINFO+x}" ]; then
-  BUILD_MAC_SILICON_SCRIPT_PATH=${0:a:h}
+  BUILD_WINDOWS_SCRIPT_PATH=${0:a:h}
 else
-  BUILD_MAC_SILICON_SCRIPT_PATH=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+  BUILD_WINDOWS_SCRIPT_PATH=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 fi
 
 # Getting the Root Path of this project.
 # This is so that we can go there and compile our Go Module from the root path.
-PROJECT_ROOT_PATH=$(dirname ${BUILD_MAC_SILICON_SCRIPT_PATH})
+PROJECT_ROOT_PATH=$(dirname ${BUILD_WINDOWS_SCRIPT_PATH})
 cd ${PROJECT_ROOT_PATH}
 
 # Now compiling for windows
-echo "Compiling for Mac Os Silicon while in directory : ${PROJECT_ROOT_PATH}"
+echo "Currently in path : ${PROJECT_ROOT_PATH}"
+
+echo "Compiling for Windows OS x64"
+env GOOS=windows GOARCH=amd64 go build .
+
+echo "Compiling for Mac OS M-Series Silicon"
 env GOOS=darwin GOARCH=arm64 go build -o .
 
-echo "Compilation done and ready for distribution"
+echo "Compilation done"
